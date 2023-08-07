30856e2481c5b04d78775fe2416cf80f06a40d35
Yumilify <101920479+Yumilify@users.noreply.github.com>
2023-08-07 11:45:28 -0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\n Hello world! \n\n " << endl;
+     cout << "\n Hello from planet earth\n " << endl;
+    return 0;
+}
